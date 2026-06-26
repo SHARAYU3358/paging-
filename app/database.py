4ee9464c74ee32be56fe880c76206329e01dfd7f
Sharayu Tathe <sharayutathe@gmail.com>
2026-06-26 07:57:45 +0530

    Update database connection

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Sharayu3@localhost/codevector"
+DATABASE_URL = "mysql+pymysql://root:password@localhost/codevector"
 
 engine = create_engine(
     DATABASE_URL,
